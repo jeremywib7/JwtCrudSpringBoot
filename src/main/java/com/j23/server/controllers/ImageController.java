b197c2962492144f0ac0d17160f0fca31868dc41
Jeremy <jeremywib7@gmail,com>
2022-01-10 00:56:46 +0700

    upload image from api

--- src/main/java/com/j23/server/controllers/ImageController.java
+++ src/main/java/com/j23/server/controllers/ImageController.java
@@ -1,19 +1,25 @@
 package com.j23.server.controllers;
 
+import com.j23.server.models.auth.User;
+import com.j23.server.services.auth.JwtService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.stream.Collectors;
 
 @RestController
 @CrossOrigin
 @RequestMapping("/images")
 public class ImageController {
+
+    private User user;
 
     @PostMapping
     public ResponseEntity<?> uploadImage(@RequestParam("file") MultipartFile file) {
@@ -22,18 +28,33 @@ public class ImageController {
             throw new RuntimeException("File given is  not valid");
         }
 
-        String folder = "C:\\Users\\ADMIN14-SB1\\Pictures\\uploads\\";
+        String folder = "D:\\ImageData\\"; // my file path
 
         try {
             Path pathFolder = Paths.get(folder);
             Files.createDirectories(pathFolder);
+            Path pathFile = Paths.get(folder + file.getOriginalFilename());
 
-            Path pathFile =  Paths.get(folder + file.getOriginalFilename());
             Files.write(pathFile, file.getBytes());
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
 
-        return new ResponseEntity(HttpStatus.OK);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
+
+//    public static String getResourceFileAsString(String folder) {
+//        InputStream is = getResourceFileAsInputStream(folder);
+//        if (is != null) {
+//            BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+//            return (String)reader.lines().collect(Collectors.joining(System.lineSeparator()));
+//        } else {
+//            throw new RuntimeException("resource not found");
+//        }
+//    }
+//
+//    public static InputStream getResourceFileAsInputStream(String folder) {
+//        ClassLoader classLoader = ImageController.class.getClassLoader();
+//        return classLoader.getResourceAsStream(folder);
+//    }
 }
