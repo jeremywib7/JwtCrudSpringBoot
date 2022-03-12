981f898c99565eae2fe7f7318f2550807f318b2e
jeremywib <jeremywib7@gmail.com>
2022-03-12 08:50:24 +0700

    dynamic report

--- src/main/java/com/j23/server/controllers/ReportController.java
+++ src/main/java/com/j23/server/controllers/ReportController.java
@@ -6,6 +6,9 @@ import com.j23.server.services.product.ProductService;
 import net.sf.jasperreports.engine.*;
 import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +16,8 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.servlet.http.HttpServletResponse;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
 
 @RestController
@@ -20,27 +25,28 @@ import java.util.HashMap;
 public class ReportController {
 
     @Autowired
-    private ReportService reportService;
-
-    @Autowired
     UserService userService;
 
     @Autowired
     ProductService productService;
 
-    @Autowired
-    private HttpServletResponse httpServletResponse;
-
     @GetMapping("/user/list")
-    public String generatePdf() throws FileNotFoundException, JRException {
+    public ResponseEntity<byte[]> generatePdf() throws FileNotFoundException, JRException {
+
         JRBeanCollectionDataSource beanCollectionDataSource = new JRBeanCollectionDataSource(userService.findAllUser());
         JasperReport jasperReport = JasperCompileManager.compileReport(new FileInputStream(
                 "src/main/resources/reports/User.jrxml"));
         HashMap<String, Object> map = new HashMap<>();
         JasperPrint report = JasperFillManager.fillReport(jasperReport, null, beanCollectionDataSource);
-        JasperExportManager.exportReportToPdfFile(report, "invoice.pdf");
+//        JasperExportManager.exportReportToPdfFile(report, "invoice.pdf");
 
-        return beanCollectionDataSource.toString();
+        byte[] data = JasperExportManager.exportReportToPdf(report);
+
+        String localDateTime = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+        HttpHeaders headers = new HttpHeaders();
+        headers.set(HttpHeaders.CONTENT_DISPOSITION, "inline;filename=" + "User List_" + localDateTime + ".pdf");
+
+        return ResponseEntity.ok().headers(headers).contentType(MediaType.APPLICATION_PDF).body(data);
     }
 
 }
