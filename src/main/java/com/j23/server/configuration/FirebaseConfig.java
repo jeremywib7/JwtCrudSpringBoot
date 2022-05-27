a9fac8dfc49865c68d0c4f8f4e773998e87e7259
jeremywib <jeremywib7@gmail.com>
2022-05-27 21:51:09 +0700

    change firebase config

--- src/main/java/com/j23/server/configuration/FirebaseConfig.java
+++ src/main/java/com/j23/server/configuration/FirebaseConfig.java
@@ -3,6 +3,7 @@ package com.j23.server.configuration;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ResourceUtils;
 
@@ -10,25 +11,28 @@ import javax.annotation.PostConstruct;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.Objects;
 
 @Service
 public class FirebaseConfig {
 
-  @PostConstruct
-  public void configureFirebaseConnection() throws IOException {
+    @PostConstruct
+    public void configureFirebaseConnection() throws IOException {
 
-    File file = ResourceUtils.getFile("classpath:config/serviceAccountKey.json");
+//    File file = ResourceUtils.getFile("classpath:config/serviceAccountKey.json");
 
-    FileInputStream serviceAccount = new FileInputStream(file);
+        File file = new File(Objects.requireNonNull(getClass().getResource("config/serviceAccountKey.json")).getFile());
 
-    FirebaseOptions options = FirebaseOptions.builder()
-      .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-      .setDatabaseUrl("https://self-service-4820d-default-rtdb.asia-southeast1.firebasedatabase.app")
-      .build();
+        FileInputStream serviceAccount = new FileInputStream(file);
 
-    if (FirebaseApp.getApps().isEmpty()) { //<--- check with this line
-      FirebaseApp.initializeApp(options);
+        FirebaseOptions options = FirebaseOptions.builder()
+                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                .setDatabaseUrl("https://self-service-4820d-default-rtdb.asia-southeast1.firebasedatabase.app")
+                .build();
+
+        if (FirebaseApp.getApps().isEmpty()) { //<--- check with this line
+            FirebaseApp.initializeApp(options);
+        }
+
     }
-
-  }
 }
