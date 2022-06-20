62cd4c728a7f4bfec9a6edae8a04f839e8bd54e8
Jeremy <jeremywib7@gmail,com>
2022-06-20 11:57:17 +0700

    update admin and client url

--- src/main/java/com/j23/server/configuration/CrossOriginConfiguration.java
+++ src/main/java/com/j23/server/configuration/CrossOriginConfiguration.java
@@ -13,7 +13,7 @@ public class CrossOriginConfiguration implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:4200", "http://127.0.0.1:4200", "http://localhost:4201",
-                        "http://127.0.0.1:4201", "https://self-service-4820d.web.app")
+                        "http://127.0.0.1:4201", "https://myselfservice.firebaseapp.com", "https://adminselfservice.firebaseapp.com")
                 .allowedMethods("GET", "PUT", "POST", "DELETE")
 //                .allowedHeaders("Origin", "Access-Control-Allow-Origin", "Content-Type",
 //                        "Accept", "Jwt-Token", "Authorization", "Origin, Accept", "X-Requested-With",
