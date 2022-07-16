2d5c3f7d551916f9d5db7437e5ec53c15a699f61
Jeremy Yonathan <jeremywib7@gmail.com>
2022-07-16 17:10:57 +0700

    work on qna delete

--- src/main/java/com/j23/server/services/restaurant/qna/QnaService.java
+++ src/main/java/com/j23/server/services/restaurant/qna/QnaService.java
@@ -4,10 +4,13 @@ import com.j23.server.controllers.exception.ConflictException;
 import com.j23.server.models.qna.QnA;
 import com.j23.server.repos.qna.QnaRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.UUID;
 
@@ -46,6 +49,10 @@ public class QnaService {
   }
 
   public void deleteQna(UUID id) {
-    qnaRepository.deleteById(id);
+    try {
+      qnaRepository.deleteById(id);
+    } catch (EmptyResultDataAccessException exception) {
+      throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Qna id not found");
+    }
   }
 }
