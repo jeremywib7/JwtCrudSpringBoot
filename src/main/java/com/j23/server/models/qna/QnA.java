f98976cef328faaf9b10a6d132715be28258bfdb
Jeremy <jeremywib7@gmail,com>
2022-07-22 00:44:59 +0700

    update logging

--- src/main/java/com/j23/server/models/qna/QnA.java
+++ src/main/java/com/j23/server/models/qna/QnA.java
@@ -1,27 +1,32 @@
 package com.j23.server.models.qna;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import com.j23.server.models.utils.BaseModel;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.SQLDelete;
-import org.hibernate.annotations.Type;
-import org.hibernate.annotations.Where;
+import org.hibernate.annotations.*;
 
 import javax.persistence.*;
+import javax.persistence.Entity;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@EqualsAndHashCode(callSuper = true) // for extends base model
-@SQLDelete(sql = "UPDATE qna a SET a.deleted_on = CURRENT_TIMESTAMP, number = null  WHERE id=?")
-@Where(clause = "deleted_on is null")
-public class QnA extends BaseModel {
+//@EqualsAndHashCode(callSuper = true) // for extends base model
+//@SQLDelete(sql = "UPDATE qna a SET a.deleted_on = CURRENT_TIMESTAMP, number = null  WHERE id=?")
+//@Where(clause = "deleted_on is null")
+public class QnA {
   @Id
   @Type(type = "uuid-char")
   @GeneratedValue(strategy = GenerationType.AUTO)
@@ -31,11 +36,25 @@ public class QnA extends BaseModel {
   @Column()
   private Integer number;
 
-  @Column(length = 100, unique = true, nullable = false)
+  @Column(length = 100, nullable = false)
   @NotBlank(message = "Question is mandatory")
   private String question;
 
   @Column(length = 200, nullable = false)
   @NotBlank(message = "Answer is mandatory")
   private String answer;
+
+  @CreationTimestamp
+  @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+  @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+  @JsonSerialize(using = LocalDateTimeSerializer.class)
+  @Column(name = "date_created", updatable = false)
+  private LocalDateTime createdOn;
+
+  @UpdateTimestamp
+  @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+  @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+  @JsonSerialize(using = LocalDateTimeSerializer.class)
+  @Column(name = "last_updated")
+  private LocalDateTime updatedOn;
 }
